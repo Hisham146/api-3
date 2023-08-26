f79206662075163a90ae9ab29a47a208413fd63d
Hisham146 <99106513+Hisham146@users.noreply.github.com>
2023-08-26 16:40:47 +0500

    Update ad.route.js

--- api/routes/ad.route.js
+++ api/routes/ad.route.js
@@ -4,13 +4,14 @@ import {
   deleteAd,
   getAd,
   getAds,
+  userMyAds,
   updateAd,getuserAds,admingetAds,admindeleteAd, adminSelectedDeleteAd // Import the updateAd controller function
 } from "../controllers/ad.controller.js";
 
 const router = express.Router();
 
 router.post("/postAd/:id",  createAd);
-router.delete("/:id", deleteAd);
+router.delete("/delete/:id", deleteAd);
 router.delete("/admin/selected/:ids",  adminSelectedDeleteAd);
 router.delete("/admin/:id", admindeleteAd);
 router.get("/single/:id", getAd);
@@ -18,5 +19,6 @@ router.get("/", getAds);
 router.get("/adminposts", admingetAds);
 router.put("/:id", updateAd);
 router.get("/userposts/:id", getuserAds);
+router.get("/myposts/:id", userMyAds);
 
 export default router;
